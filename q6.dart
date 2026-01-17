0e987c4f74740fbf6c351a1d42001035a00d5c8d
Hadi Heikal <hadiiheikal@gmail.com>
2026-01-17 18:19:49 +0200

    third commit

---
+++ q6.dart
@@ -0,0 +1,6 @@
+// Write a Dart program that declares a variable with a double data type, assigns it a value of 12.5, and prints it to the console.
+
+void main() {
+  double x = 12.5;
+  print("the value of x is : $x");
+}
