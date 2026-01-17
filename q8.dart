91c59bfa9ee774e8fc4e07dc4775e39fcf627dd8
Hadi Heikal <hadiiheikal@gmail.com>
2026-01-17 18:26:20 +0200

    4th commit

---
+++ q8.dart
@@ -0,0 +1,8 @@
+// What is the difference between the num and double data types in Dart?
+
+/*
+-> num is a parent (super) type that can hold both integers and decimal numbers. 
+-> It can store values of type int or double.
+
+double is a specific type used only for decimal (floating-point) numbers.
+*/
